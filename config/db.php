f18b92d5eddb4ed597ecddad0e2c070d0fb0c2dc
Andrii Rybak <rybak.andriy@ukr.net>
2020-07-24 13:16:57 +0300

    Docker-compose edited, root password for adminer-contrainer set

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=kgdb',
     'username' => 'root',
-    'password' => '',
+    'password' => '123456',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
